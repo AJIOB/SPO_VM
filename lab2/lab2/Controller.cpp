1bcc5a541122127fa95611ad6928050c4f0c5776
vtrusevich <vtrusevich@gmail.com>
2017-03-09 20:37:36 +0300

    lab2: Добавлена логика для coffeMachine под win32(без компилляции)

--- lab2/lab2/Controller.cpp
+++ lab2/lab2/Controller.cpp
@@ -8,7 +8,22 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+#else
+#error Bad operation system. Please, recompile me to Linux, Unix or Windows
 #endif
+
+
+#ifdef _WIN32
+namespace
+{
+	static const LPCTSTR isMachineFree = TEXT("isMachineFree");
+	static const LPCTSTR fromUser = TEXT("fromUser");
+	static const LPCTSTR fromMachine = TEXT("fromMachine");
+}
+#elif (defined(__linux__) || defined(__unix__))
+	//todo linux
+#endif
+
 
 #include "Controller.h"
 
@@ -46,8 +61,6 @@ void SelectMode()
 	//todo Windows
 #elif (defined(__linux__) || defined(__unix__))
 	//todo linux
-#else
-#error Bad operation system. Please, recompile me to Linux, Unix or Windows
 #endif
 
 void WorkAsPerson()
@@ -72,18 +85,68 @@ void WorkAsPerson()
 
 void WorkAsCoffeeMachine() //TVS
 {
+#ifdef _WIN32
 	//TODO 
-	//check existing 
+	
+	Handle* event[3];
+	
+	//check existing
+	if(event[0]=OpenEvent(null,null,isMachineFree))
+	{
+		std::cout<<"Îøèáêà! Àâòîìàò óæå çàïóùåí.";		//error. Machine already started
+		CloseHandle(event[0]);
+		return;
+	}
+	else if(event[1]=OpenEvent(null,null,fromUser))
+	{
+		std::cout<<"Îøèáêà! Àâòîìàò óæå çàïóùåí.";		//error. Machine already started
+		CloseHandle(event[1]);
+		return;
+	}
+	else if(event[2]=OpenEvent(null,null,fromMachine))
+	{
+		std::cout<<"Îøèáêà! Àâòîìàò óæå çàïóùåí.";		//error. Machine already started
+		CloseHandle(event[2]);
+		return;
+	}
+	
 	//create events
+	event[0]=CreateEvent(null,false,false,isMachineFree);
+	event[1]=CreateEvent(null,false,false,fromUser);
+	event[2]=CreateEvent(null,false,false,fromMachine);
+	
+	
 	CoffeMachine machine;
 	do
 	{
 		//raise flag1
+		if(!pulseEvant(event[0]))
+		{
+			std::cout<<"Îøèáêà! Ñîáûòèå íå ñîçäàíî";		//error. Event is not pulsed
+			return;
+		}
+		
 		// wait	flag2
+		waitForSingleObject(event[1],INFINITE);
+		
 		machine.proceed();
+		
 		//raise flag3
+		if(!pulseEvant(event[2]))
+		{
+			std::cout<<"Îøèáêà! Ñîáûòèå íå ñîçäàíî";		//error. Event is not pulsed
+			return;
+		}
+		
 		//wait flag2
+		waitForSingleObject(event[1],INFINITE);
+		
 		machine.writeToFile();
 	}
 	while(true);
+	
+#elif (defined(__linux__) || defined(__unix__))
+	//todo linux
+#endif
+	
 }
