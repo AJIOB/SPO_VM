2d9d774a84fbc7bc5c18e02a62a4a541e5db682e
AJIOB <aleksandr9809@gmail.com>
2017-03-23 19:37:02 +0300

    lab3: AJIOBlib & Constants are returned.

--- lab3/Constants.h
+++ lab3/Constants.h
@@ -1,16 +1,32 @@
 #ifndef VA_CONSTANTS
 #define VA_CONSTANTS
 
+#ifdef _WIN32
+#include <Windows.h>
+#endif
+
 namespace VA
 {
-	namespace constants
-	{
-		static const char transferFile[] = "transfer.db";
+    namespace constants
+    {
+        static const char transferFile[] = "transfer.db";
 
-		static const char serverPIDfilename[] = "serverPID.db";
-		static const int SIGF0 = 10;
-		static const int SIGF1 = 12;
-	}
+#ifdef _WIN32
+
+        static const LPCTSTR isMachineFree = TEXT("isMachineFree");
+		static const LPCTSTR fromUser = TEXT("fromUser");
+		static const LPCTSTR fromMachine = TEXT("fromMachine");
+		static const LPCTSTR disconnectUser = TEXT("disconnectUser");
+
+#elif (defined(__linux__) || defined(__unix__))
+
+        static const char serverPIDfilename[] = "serverPID.db";
+        static const int SIGF0 = 10;
+        static const int SIGF1 = 11;
+        static const int SIGF2 = 12;
+
+#endif
+    }
 }
 
 #endif
