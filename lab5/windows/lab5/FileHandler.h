fee1dd2f274d883395575d3618e08c12458e3d83
vtrusevich <vtrusevich@gmail.com>
2017-04-11 22:31:10 +0300

    lab5: Все работает (без dll)

---
+++ lab5/windows/lab5/FileHandler.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <windows.h>
+#include <tchar.h> 
+#include <stdio.h>
+#include <iostream>
+#include <strsafe.h>
+class FileHandler
+{
+private:
+
+public:
+	
+	char subDirs[MAX_PATH][100];
+	char* mainDir;
+	
+	FileHandler(char* mainDir);
+	~FileHandler(void);
+	bool getDirectories();
+	bool getFiles();
+	bool createFileThreads(char* directory,char*file, LARGE_INTEGER ilesize,char* rootDir);
+	//static DWORD WINAPI runWriter(LPVOID lpParam);
+
+	int subDirsCount;
+};
+
